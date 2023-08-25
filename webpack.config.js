81bf98b616ad0b69ea002b13dd1c4d574c304eb9
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-08-25 09:09:36 +0200

    The two wagons are ready to race but still static

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     path: path.resolve(__dirname, 'dist'), 
-    filename: 'bundle.js', 
+    filename: 'main.js', 
   },
 };
