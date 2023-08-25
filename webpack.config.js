2d1aa372dfa03fdd84e7058bfe1fae1678040988
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-08-25 19:05:49 +0200

    First player can make a move and win the game

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
+
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, "./lib/wagon_race.js"), 
+  entry: path.resolve(__dirname, "lib/wagon_race.js"), 
   mode: "development",
   output: {
     path: path.resolve(__dirname, "dist"),
-    path: path.resolve(__dirname, 'dist'), 
     filename: 'main.js', 
+    publicPath: '/'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'), // Point to the "public" directory
+    },
+    port: 8080,
   },
 };
